e02475da4d610e5943f398e5ea7c23b62d584c68
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2023-05-20 23:11:21 +0530

    Time: 68 ms (68.47%), Space: 48.3 MB (94.47%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if(nums.size()==1)
+            return true;
+        if(nums[0]==0)
+            return false;
+        int dist=0;
+        int pos=0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==0 && dist==i)
+            {
+                return false;
+            }
+            dist=max(dist,nums[i]+i);
+            if(pos==i)
+            {
+                pos=dist;
+            }
+        }
+        return true;
+    }
+};
