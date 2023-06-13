c4bd5e35b85234778300ded7c9860b4ca27a1d39
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2023-06-14 04:23:45 +0530

    Time: 593 ms (6.12%), Space: 178.5 MB (23.56%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,37 +1,76 @@
 class LRUCache
 {
     public:
-        list<pair<int,int>> l;
-        unordered_map<int,list<pair<int, int>>::iterator> m;
-        int size;
-        LRUCache(int capacity)
-        {
-            size=capacity;
-        }
-        int get(int key)
-        {
-            if(m.find(key)==m.end())
-                return -1;
-            l.splice(l.begin(),l,m[key]);
-            return m[key]->second;
-        }
-        void put(int key, int value)
-        {
-            if(m.find(key)!=m.end())
+        class node{
+            public:
+            int key;
+            int val;
+            node*next;
+            node*prev;
+            node(int _key, int _value)
             {
-                l.splice(l.begin(),l,m[key]);
-                m[key]->second=value;
-                return;
+                key= _key;
+                val= _value;
             }
-            if(l.size()==size)
-            {
-                auto d_key=l.back().first;
-                l.pop_back();
-                m.erase(d_key);
-            }
-            l.push_front({key,value});
-            m[key]=l.begin();
+        };
+    
+    node* head = new node(-1,-1);//Initialized. 
+    node* tail = new node(-1,-1);
+    int max_cap;
+    unordered_map<int,node*>m;//int , Address is stored. 
+    
+    LRUCache(int capacity)//Initial config. 
+    {
+        max_cap=capacity;
+        head->next=tail;
+        tail->prev =head;
+    }
+    
+    void addnode(node* newnode)
+    {
+        node* temp = head->next;
+        newnode->next = temp;
+        newnode->prev = head;
+        head->next = newnode;
+        temp->prev = newnode;
+    }
+    void deletenode(node* delnode)
+    {
+        node*delprev = delnode->prev;
+        node*delnext= delnode->next;
+        delprev->next= delnext;
+        delnext->prev = delprev;
+    }
+    int get(int key_)
+    {
+       if (m.find(key_) != m.end())//I.e it exists in the map. 
+       {
+           node*resnode = m[key_];//Address. 
+           int res = resnode->val;
+           m.erase(key_);
+           deletenode(resnode);
+           addnode(resnode);//Right after head. 
+           m[key_] = head->next;//New addr. updated. 
+           return res;
+       }
+        return -1;// Because it did not find it. 
+    }
+    void put(int key_,int value)
+    {
+        if(m.find(key_)!=m.end())
+        {
+            node*existingnode = m[key_];
+            m.erase(key_);
+            deletenode(existingnode);
         }
+        if(m.size() == max_cap)
+        {
+            m.erase(tail->prev->key);
+            deletenode(tail->prev);
+        }
+        addnode(new node(key_,value));
+        m[key_] = head->next;
+    }
 };
 
 /**
