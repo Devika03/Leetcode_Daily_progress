d7ce3b6f638aa00f2640a9f9715473dd314aa2fa
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2023-07-13 20:04:25 +0530

    Add files via upload

---
+++ Graph - numofDistinctIslands.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+    private:
+    void dfs(int row , int col , vector<vector<int>> &vis , vector<vector<int>> &grid,vector<pair<int,int>>&vec , int row0 , int col0 )
+    {
+        vis[row][col]=1;
+        vec.push_back({row-row0,col-col0});//Sub. from base co-od. 
+        int m = grid.size();
+        int n = grid[0].size();
+        int drow[]={-1,0,1,0};
+        int dcol={0,-1,0,1};
+        for(int i = 0 ; i<4 ; i++)
+        {
+            int nrow = row +drow[i];
+            int ncol = col + dcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]==1)
+            {
+                dfs(nrow,ncol,vis,grid,vec,row0,col0);
+            }
+        }
+    }
+    public:
+    int numIslands(vector<vector<int>> &grid)
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>>vis(n, vector<int>(m,0));
+        set<vector<pair<int,int>>st;
+        for(int i =0 ; i<n;i++)
+        {
+            for(int j = 0 ; j<m ; j++)
+            {
+                if(!vis[i][j] &&  grid[i][j]==1)
+                {
+                    vector<pair<int,int>>vec;
+                    dfs(i,j,grid,vis);
+                    st.insert(vec);
+                }
+            }
+        }
+        return st.size();
+    }
+}
