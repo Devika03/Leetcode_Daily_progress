98e5deb627e2c266541c7e3149eb8b679e5935b3
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2023-08-24 00:54:37 +0530

    Time: 10 ms (77.92%), Space: 12.6 MB (48.03%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,20 +1,20 @@
 class Solution {
-    static bool cmp(pair<string,int> p1,pair<string,int> p2)
+    static bool cmp(pair<string,int>p1 ,pair<string,int>p2)
     {
-        if(p1.second > p2.second || p1.second==p2.second && p1.first< p2.first)
+        if(p1.second > p2.second || p1.second==p2.second && p1.first< p2.first/*Lexico. check*/)
         return true;
         return false;
     }
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string,int> mp;
+        unordered_map<string,int> mp; //To create the Map and store the freq.
         int n=words.size();
         for(int i=0;i<n;i++)
         {
             mp[words[i]]++;
         }
-        vector<pair<string ,int>> v(mp.begin(),mp.end());
-        sort(v.begin(),v.end(),cmp);
+        vector<pair<string ,int>> v(mp.begin(),mp.end());//Init. v w/ the contents of mp.
+        sort(v.begin(),v.end(),cmp);//Sorts Lexico.
        
         vector<string> ans;
        for(int i=0;i<k;i++)
