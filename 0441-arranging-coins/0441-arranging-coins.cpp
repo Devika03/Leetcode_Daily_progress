8e943c75098851fe7d790321f291089dc0c2c180
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2023-08-23 23:23:59 +0530

    Time: 3 ms (72.22%), Space: 6 MB (43.45%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int arrangeCoins(int n) 
+    {
+        long long val = (long long) n;
+        long long t1 = (-1 + sqrt(1 + 8*val)) / 2; 
+        return t1; 
+    }
+};
