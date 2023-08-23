8bdfad35b1df39360655acb2e69f57f4df22ec3c
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2023-08-23 23:25:07 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (10.88%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,9 +1,36 @@
-class Solution {
+class Solution 
+{
 public:
+    vector<long long> cost(int x)
+    {
+        long long tmp = (long long) x;
+        long long steps = tmp * (tmp + 1) / 2; 
+        vector<long long > res = {steps, x}; 
+        return res; 
+    }
     int arrangeCoins(int n) 
     {
-        long long val = (long long) n;
-        long long t1 = (-1 + sqrt(1 + 8*val)) / 2; 
-        return t1; 
+        int left = 1,  right = n; 
+        int ans = 0; 
+
+        while( left <= right)
+        {
+            long long pivot = left + (right - left) / 2; 
+            vector<long long> ans = cost(pivot); 
+            if(ans[0] == n)
+                return ans[1]; 
+            else if(ans[0] < n)
+                left = pivot + 1; 
+            else 
+                right = pivot - 1; 
+        }
+
+        // Checking if it's complete or not.
+        vector<long long> res = cost(left); 
+        if(res[0] == n)
+            return left;
+        else
+            return left - 1;  
+        return left; 
     }
 };
