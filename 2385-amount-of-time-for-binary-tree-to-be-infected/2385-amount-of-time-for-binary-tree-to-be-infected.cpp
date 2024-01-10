c6650df917dc76ed106ddacefce99cc315f762ef
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-01-10 15:45:06 +0400

    Time: 401 ms (51.05%), Space: 339.8 MB (5.09%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,70 +1,56 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void solve(TreeNode* root,vector<vector<int>>&dp){
-        //base case
-        if(!root)return;
-        if(root->left){
-            dp[root->val].push_back(root->left->val);
-            dp[root->left->val].push_back(root->val);
+    int ans;
+
+    void buildGraph(TreeNode* root, vector<vector<int>>& adj) {
+        if (root == NULL) {
+            return;
         }
-        if(root->right){
-            dp[root->val].push_back(root->right->val);
-            dp[root->right->val].push_back(root->val);
+
+        if (root->left) {
+            adj[root->val].push_back(root->left->val);
+            adj[root->left->val].push_back(root->val);
         }
-        solve(root->left,dp);
-        solve(root->right,dp);
+
+        if (root->right) {
+            adj[root->val].push_back(root->right->val);
+            adj[root->right->val].push_back(root->val);
+        }
+
+        buildGraph(root->left, adj);
+        buildGraph(root->right, adj);
     }
-    int bfs(int start,vector<vector<int>>dp,set<int>&st){
-        list<vector<int>> q;
-        int cnt = 0;
-        q.push_back({start});
-        st.insert(start);
-        // while(!q.empty()){
-        //     int a = q.front();
-        //     q.pop_front();
-        //     bool flag = true;
-        //     for(auto it:dp[a]){
-        //         if(it==-1){break;}
-        //         else if(st.find(it)==st.end()){st.insert(it);q.push_back(it);if(flag ==true){cnt++;flag = false;}}
-        //         else{}
-        //     }
-        // }
-        while(!q.empty()){
-            vector<int> a = q.front();
-            for(auto it:a){
-                cout<<it<< " ";
-            }
-            cout<<endl;
-            cnt++;
-            q.pop_front();
-            vector<int> ans;
-            for(int i=0;i<a.size();i++){
-                for(auto it:dp[a[i]]){
-                    if(st.find(it)==st.end()){st.insert(it);ans.push_back(it);}
-                    else{}
+
+    void bfs(int start, vector<vector<int>>& adj, vector<bool>& vis) {
+        queue<int> q;
+        q.push(start);
+
+        while (!q.empty()) {
+            int k = q.size();
+
+            while (k--) {
+                int u = q.front();
+                q.pop();
+                vis[u] = 1;
+
+                for (int i = 0; i < adj[u].size(); i++) {
+                    if (!vis[adj[u][i]])
+                        q.push(adj[u][i]);
                 }
             }
-            if(!ans.empty())
-                    q.push_back(ans);
+            ans++;
         }
-        return cnt-1;
     }
+
     int amountOfTime(TreeNode* root, int start) {
-        vector<vector<int>> dp(1e5+1,vector<int>(1-1));
-        solve(root,dp);
-        set<int> st;
-        int a = bfs(start,dp,st);
-        return a;
+        vector<vector<int>> adj(100001);
+        vector<bool> vis(100001);
+
+        buildGraph(root, adj);
+        ans = 0;
+
+        bfs(start, adj, vis);
+
+        return ans - 1;
     }
 };
