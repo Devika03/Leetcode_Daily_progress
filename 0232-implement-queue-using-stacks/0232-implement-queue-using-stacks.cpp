e0103f76b6ea84eeac0a4fd0c9cfbd5730136ccf
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-01-29 16:06:40 +0400

    Time: 5 ms (8.25%), Space: 8.4 MB (11.04%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,25 +1,40 @@
 class MyQueue {
 public:
-    stack<int> stck;
-    stack<int> outstck;
+    stack<int> s1;
+    stack<int> s2;
+    /** Initialize your data structure here. */
+    MyQueue() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
     void push(int x) {
-        stck.push(x);
+        while(!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s2.push(x);
+        while(!s2.empty()) {
+            s1.push(s2.top());
+            s2.pop();
+        }
     }
+    
+    /** Removes the element from in front of queue and returns that element. */
     int pop() {
-        peek();
-        const int val = outstck.top();
-        outstck.pop();
-        return val;
+        int curr = s1.top();
+        s1.pop();
+        return curr;
     }
-
+    
+    /** Get the front element. */
     int peek() {
-        if (outstck.empty())
-        while (!stck.empty())
-            outstck.push(stck.top()), stck.pop();
-        return outstck.top();
+        return s1.top();
     }
+    
+    /** Returns whether the queue is empty. */
     bool empty() {
-        return stck.empty() && outstck.empty();
+        return s1.empty();
     }
 };
 
