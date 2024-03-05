729037c696d21ed6959488f1d39cb9f6b6a7d61e
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-03-06 00:01:44 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (47.47%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -1,22 +1,55 @@
 class Solution {
 public:
+    int countAliveNeighbors(vector<vector<int>>& board, int row, int col)
+    {
+        int count=0;
+        int m = board.size();
+        int n = board[0].size();
+        int drow[]={-1, -1, -1, 0, 0, 1, 1, 1};
+        int dcol[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+        for(int i =0 ; i<8 ; i++)
+        {
+            int nrow=row+drow[i];
+            int ncol=col+dcol[i];
+            if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && board[nrow][ncol] == 1)
+            count++;
+        }
+        return count;
+        }
+    
+
     void gameOfLife(vector<vector<int>>& board) 
     {
-         
-    int m = board.size(), n = m ? board[0].size() : 0;
-    for (int i=0; i<m; ++i) {
-        for (int j=0; j<n; ++j) {
-            int count = 0;
-            for (int I=max(i-1, 0); I<min(i+2, m); ++I)
-                for (int J=max(j-1, 0); J<min(j+2, n); ++J)
-                    count += board[I][J] & 1;
-            if (count == 3 || count - board[i][j] == 3)
-                board[i][j] |= 2;
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<int>>bs1(m,vector<int>(n, 0));
+        for(int i =0 ; i< m ; i++)
+        {
+            for(int j = 0 ; j< n ; j++)
+            {
+                if(board[i][j]==0)
+                {
+                    int count = countAliveNeighbors(board, i, j);
+                    if (count==3) bs1[i][j]=1;
+                }
+                else if (board[i][j]==1)
+                {
+                    int count = countAliveNeighbors(board,i,j);
+                    if(count<2)
+                    {
+                        bs1[i][j]=0;
+                    }
+                    else if (count==2 || count==3) bs1[i][j]=1;
+                    else if (count>3) bs1[i][j]=0;
+                }
+            }
         }
-    }
-    for (int i=0; i<m; ++i)
-        for (int j=0; j<n; ++j)
-            board[i][j] >>= 1;
-    
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                board[i][j] = bs1[i][j];
+            }
+        }
     }
 };
