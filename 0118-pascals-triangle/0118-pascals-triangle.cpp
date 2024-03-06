492b28afea6878243a0e5a65e44cdf198dec59ef
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-03-06 09:36:10 +0530

    Time: 2 ms (43.06%), Space: 7.8 MB (61.76%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,19 +1,22 @@
+/* i=0 [1]  here ,j=0 (A relation b/w i and j )
+   i=1 [1,1]      j=0,1
+   i=2 [1,2,1]    j=0,*1*,2
+   i=3 [1,3,3,1] */
+
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res;
-
-        for(int i = 0; i < numRows; ++i){
-            vector<int> temp(i + 1, 1);
-            for(int j = 1; j < i; ++j){
-                temp[j] = res[i - 1][j] + res[i - 1][j - 1];
+    vector<vector<int>> generate(int numRows) 
+    {
+        vector<vector<int>>res;
+        for(int i = 0 ; i<numRows ; i++)
+        {
+            vector<int>stepvec(i+1,1);//Consider you are at i=2 [1,1,1].
+            for(int j =1 ; j<i; j++)//As j=0th and j=(i+1)th ele. is set to 1. 
+            {
+                stepvec[j]=res[i-1][j-1]+res[i-1][j];
             }
-            res.push_back(temp);
+            res.push_back(stepvec);
         }
-        return res;
-    }
-
-    void printCode(vector<int> &nums){
-        for(int i = 0; i < nums.size(); ++i) cout<<nums[i]<<" ";
-    }
+return res;
+}
 };
