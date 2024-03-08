554ddeb33316642d91eed9394e939b94a76475d6
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-03-08 13:22:56 +0530

    Time: 14 ms (64.75%), Space: 20.6 MB (44.02%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -16,30 +16,33 @@ public:
 };*/
 class Solution {
 public:
-    void wiggleSort(vector<int>& nums) {
-        int n = nums.size();
-        
-        // Sort the array
-        sort(nums.begin(), nums.end());
-        
-        // Find the mid point of the array
-        int mid = (n + 1) / 2;
-        
-        // Create a temporary array to store the rearranged elements
-        vector<int> result(n);
-        
-        // Place the larger elements in even indices
-        for (int i = 0; i < n; ++i) {
-            if (i < mid) {
-                result[i * 2] = nums[mid - 1 - i];
-            }
-            // Place the smaller elements in odd indices
-            else {
-                result[(i - mid) * 2 + 1] = nums[n - 1 - (i - mid)];
-            }
+    void wiggleSort(std::vector<int>& nums) {
+
+
+        // sorting the nums
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        vector<int>temp(n);
+        int i=1;
+        int j=n-1;
+        while(i<n)
+        {
+            temp[i]=nums[j];
+            j--;
+            i+=2;
+        }
+        i=0;
+        while(i<n)
+        {
+            temp[i]=nums[j];
+            j--;
+            i+=2;
+        }
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=temp[i];
         }
         
-        // Copy the rearranged elements back to the original array
-        nums = result;
+       
     }
 };
