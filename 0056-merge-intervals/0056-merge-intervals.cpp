8fcd80361baf0aad3ffa764e19dc155738c04c91
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-03-10 18:42:31 +0530

    Time: 13 ms (98.40%), Space: 22.6 MB (74.14%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,8 +1,65 @@
-class Solution {
+/*class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& v) 
     {
-        vector<vector<int>>result;
+        int n = v.size();
+        sort(v.begin(), v.end());
+        for(int i = 1 ; i<n ; i++)
+        {
+            if(v[i][0]<=v[i-1][1])
+            {
+                v[i][0]=v[i-1][0];
+                v.erase(v.begin()+i-1);
+            }
+        }
+        return v;
+    }
+};*/
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals) {
+        if (intervals.empty()) return {};
+
+        // Sort intervals based on the start time
+        std::sort(intervals.begin(), intervals.end());
+
+        std::vector<std::vector<int>> merged;
+        merged.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (merged.back()[1] >= intervals[i][0]) {
+                // Merge overlapping intervals
+                merged.back()[1] = std::max(merged.back()[1], intervals[i][1]);
+            } else {
+                // Add new interval to the merged list
+                merged.push_back(intervals[i]);
+            }
+        }
+
+        return merged;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*vector<vector<int>>result;
         int n=v.size();
     sort(v.begin(),v.end());
     vector<vector<int>> ans;
@@ -15,5 +72,4 @@ public:
         ans.back()[1]=max(ans.back()[1],v[i][1]);
     }
     return ans;
-    }
-};
+*/
