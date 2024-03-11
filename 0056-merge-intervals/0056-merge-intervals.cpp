c4a643e1408d5a99019e49f6bb99761e4e06680b
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-03-11 14:44:00 +0530

    Time: 19 ms (88.21%), Space: 22.7 MB (36.75%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,75 +1,24 @@
-/*class Solution {
-public:
-    vector<vector<int>> merge(vector<vector<int>>& v) 
-    {
-        int n = v.size();
-        sort(v.begin(), v.end());
-        for(int i = 1 ; i<n ; i++)
-        {
-            if(v[i][0]<=v[i-1][1])
-            {
-                v[i][0]=v[i-1][0];
-                v.erase(v.begin()+i-1);
-            }
-        }
-        return v;
-    }
-};*/
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals) {
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
         if (intervals.empty()) return {};
 
         // Sort intervals based on the start time
-        std::sort(intervals.begin(), intervals.end());
+        sort(intervals.begin(), intervals.end());
 
-        std::vector<std::vector<int>> merged;
-        merged.push_back(intervals[0]);
+        vector<vector<int>> new_merged;
+        new_merged.push_back(intervals[0]);
 
-        for (int i = 1; i < intervals.size(); ++i) {
-            if (merged.back()[1] >= intervals[i][0]) {
+        for (int i = 1; i < intervals.size(); i++) {
+            if (new_merged.back()[1] >= intervals[i][0]) {
                 // Merge overlapping intervals
-                merged.back()[1] = std::max(merged.back()[1], intervals[i][1]);
+                new_merged.back()[1] = max(new_merged.back()[1], intervals[i][1]); // Consider a case of [1,4],[2,3] = [1,4] ; [1,3],[2,4] = [1,4]
             } else {
                 // Add new interval to the merged list
-                merged.push_back(intervals[i]);
+                new_merged.push_back(intervals[i]);
             }
         }
 
-        return merged;
+        return new_merged;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*vector<vector<int>>result;
-        int n=v.size();
-    sort(v.begin(),v.end());
-    vector<vector<int>> ans;
-
-    for(int i=0;i<n;i++)
-    {
-        if(ans.empty() || v[i][0]>ans.back()[1]) // ans.back() reference to last added vector. 
-        ans.push_back(v[i]);
-        else
-        ans.back()[1]=max(ans.back()[1],v[i][1]);
-    }
-    return ans;
-*/
