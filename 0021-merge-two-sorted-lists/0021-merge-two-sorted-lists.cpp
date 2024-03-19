71b195f93353cd37750936eab012fc832b64cf52
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-03-19 11:00:06 +0530

    Time: 3 ms (82.53%), Space: 18.2 MB (94.24%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -15,10 +15,49 @@ public:
     
         ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
         {
-            if(l1==NULL) return l2;
+          if(l1==NULL)return l2;
+          if(l2==NULL)return l1;
+          ListNode*temp=new ListNode(0);
+          ListNode*dummy=temp;  
+          while(l1!=NULL && l2!=NULL)
+          {
+              if(l1->val<=l2->val)
+              {
+                  temp->next=l1;
+                  l1=l1->next;
+              }
+              else{
+              temp->next = l2;
+              l2=l2->next;
+              }
+              temp=temp->next;
+          }
+            if(l1==NULL)
+            {
+                while(l2!=NULL)
+                {
+                temp->next= l2;
+                temp=temp->next;
+                l2=l2->next;
+                }
+            }
+            if(l2==NULL)
+            {
+                while(l1!=NULL)
+                {
+                    temp->next=l1;
+                    temp=temp->next;
+                    l1=l1->next;
+                }
+            }
+           return dummy->next;
+            
+        }
+};
+/* if(l1==NULL) return l2;
             if(l2==NULL) return l1;
-            if(l1->val > l2->val) std::swap(l1,l2);
-            ListNode*res=l1;//if(l1->val > l2->val)
+            if(l1->val > l2->val) std::swap(l1,l2);//This is done to ensure that l1 always starts with the smaller value.
+            ListNode*res=l1;//if(l1->val < l2->val) ; res is a copy of l1 and it'll be final ans. 
             while(l1 != NULL && l2 !=NULL)
             {
                 ListNode*temp = NULL ; 
@@ -32,5 +71,4 @@ public:
                 
             }
             return res;
-        }
-};
+            */
