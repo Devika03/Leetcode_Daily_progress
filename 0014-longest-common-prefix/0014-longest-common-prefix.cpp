760adb839c7732b2119534c29348a05bb4593b27
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-05-09 00:07:56 +0400

    Time: 7 ms (28.44%), Space: 11.5 MB (15.17%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& v) {
-        string ans="";
-        sort(v.begin(),v.end());
-        int n=v.size();
-        string first=v[0],last=v[n-1];
-        for(int i=0;i<min(first.size(),last.size());i++){
-            if(first[i]!=last[i]){
-                return ans;
-            }
-            ans+=first[i];
+    string longestCommonPrefix(vector<string>& strs) 
+    {
+        sort(strs.begin() , strs.end());
+        string result = "";
+        if (strs.empty()) 
+        {
+            return result; // Return empty string for empty input
         }
-        return ans;
+        int n = strs.size();
+        string first_word= strs[0];
+        string last_word= strs[n-1];
+        int size_first_word = first_word.size();
+        int size_last_word = last_word.size();
+        int min_size = min(size_first_word, size_last_word);
+        for(int i = 0 ; i< min_size ; i++ )
+        {
+            if(first_word[i]==last_word[i])
+            {
+                result = result+first_word[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+        return result;
+        
+        
     }
-};
+    };
