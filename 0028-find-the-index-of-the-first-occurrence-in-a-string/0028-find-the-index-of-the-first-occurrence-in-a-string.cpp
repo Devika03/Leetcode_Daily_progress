40c21d41caebf96969bc69d012c8b2869bea6813
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-05-09 19:35:52 +0400

    Time: 3 ms (41.71%), Space: 7.6 MB (50.56%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,21 +1,27 @@
+#include<string.h>
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int n = haystack.length();
-        int m = needle.length();
-        
-        if (m == 0) return 0; // empty needle
-        
-        for (int i = 0; i <= n - m; i++) {
-            bool found = true;
-            for (int j = 0; j < m; j++) {
-                if (haystack[i+j] != needle[j]) {
-                    found = false;
-                    break;
-                }
-            }
-            if (found) return i;
+    int strStr(string haystack, string needle) 
+    {
+        if ( haystack.find(needle) != string::npos )
+        {
+            return haystack.find(needle);
         }
-        return -1; // needle not found
+        else 
+            return -1;
     }
+    
 };
+
+/*
+int n1 = haystack.size();//9
+        int n2 = needle.size();//3
+        
+        if (n2>n1) return -1; // Corner case .
+        
+        for (int i = 0; i < n1-n2+1; i++)//haystack
+        {
+            if(haystack.substr(i,n2)==needle) { return i; }//substr with start index = i and end index = n2.
+        }
+            return -1; // needle not found
+*/
