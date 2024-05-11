50c114dfdfebed2a5a5e00cf25da7c78949c2e7a
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-05-11 13:47:48 +0400

    Time: 3 ms (43.74%), Space: 7.8 MB (17.75%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,8 +1,79 @@
 class Solution {
 public:
+    /*vector<int> splitString(string &s , char delimeter)
+    {
+        vector<int>ans;
+        int start=0;
+        int end = s.find(delimeter);
+        while(end != string::npos)
+        {
+            string temp = s.substr(start , end - start);
+            ans.push_back(stoi(temp));
+            start=end+1;
+            end = s.find(delimeter);
+        }
+        string temp = s.substr(start, end-start);
+        ans.push_back(stoi(temp));
+        return ans;
+    }*/
+    vector<int> splitString(const string& s, char delimiter) {
+        vector<int> result;
+        stringstream ss(s);
+        string token;
+        while (getline(ss, token, delimiter)) {
+            result.push_back(stoi(token));
+        }
+        return result;
+    }
+    
     int compareVersion(string version1, string version2) 
     {
-        int i = 0, j = 0;
+        vector<int>v1 = splitString( version1 , '.');
+        vector<int>v2 = splitString( version2 , '.');
+        int val1 , val2;
+        for(int i =0 ; i<max(v1.size(),v2.size()); i++)
+        {
+            if (i < v1.size()) 
+            {
+                val1 = v1[i];
+            } 
+            else 
+            {
+                val1 = 0;
+            }
+
+            if (i < v2.size()) 
+            {
+                val2 = v2[i];
+            } 
+            else 
+            {
+                val2 = 0;
+            }
+            if(val1<val2) return -1;
+            else if(val1 >val2) return 1;
+        }
+        return 0;
+             
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ int i = 0, j = 0;
         while (i < version1.length() || j < version2.length()) {
             int num1 = 0, num2 = 0;
             while (i < version1.length() && version1[i] != '.') {
@@ -18,5 +89,4 @@ public:
             if(num1!=num2)return (num1>num2)?1:-1;
         }  
         return 0;
-    }
-};
+*/
