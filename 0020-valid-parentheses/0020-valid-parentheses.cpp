a293bfc8ecf81f3c39d94afee12f8e5117f68fc7
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-05-28 12:49:49 +0400

    Time: 0 ms (100.00%), Space: 7.5 MB (85.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,14 +1,56 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<char> stk;
-        for (char c : s) {
-            if (c == '(' || c == '{' || c == '[') {
-                stk.push(c);
-            } else {
-                if (stk.empty()) {
-                    return false;
+    bool isValid(string s) 
+    {
+        stack<char>stack;
+        for(char c : s)
+        {
+            if(c == '(' || c == '[' || c == '{' )
+            {
+                stack.push(c);
+            }
+            else 
+            {
+                if(stack.empty()) return false;
+                
+                char stack_top_Ele = stack.top();
+                if(c == ')' && stack_top_Ele == '(' || c == ']' && stack_top_Ele == '[' || c == '}' && stack_top_Ele == '{' ) stack.pop();
+                else return false;                
+            }
+                   
+        }
+        return stack.empty();
+    }
+        
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+{stack<char> s;
+        for (char c : s) //Iterating through string. 
+        {
+            if (c == '(' || c == '{' || c == '[') s.push(c);
+            else 
+            {
+                if (stk.empty()) 
+                {
+                    return false; //It means it does not have an opp. bracket
                 }
+                //If it does have an opp. bracket then 3 cases - 
                 if (c == ')' && stk.top() == '(') {
                     stk.pop();
                 } else if (c == '}' && stk.top() == '{') {
@@ -22,4 +64,4 @@ public:
         }
         return stk.empty();
     }
-};
+    */
