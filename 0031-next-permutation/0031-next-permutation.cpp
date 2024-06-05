1ee20e43e825467a1d22b899e1bf332f3ca0785e
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-06-05 12:27:57 +0400

    Time: 0 ms (100.00%), Space: 14.6 MB (53.84%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,4 +1,4 @@
-/*class Solution {
+class Solution {
 public:
     void nextPermutation(vector<int>& nums) 
     {
@@ -15,6 +15,7 @@ public:
         if(index==-1)
         {
             reverse(nums.begin(), nums.end());
+            return;
         }
         for(int j = n-1 ; j>=index ; j--)
         {
@@ -27,37 +28,4 @@ public:
         reverse(nums.begin() + index+1, nums.end() );
         
             }
-}; */
-class Solution {
-public:
-    void nextPermutation(vector<int>& nums) 
-    {
-        int n = nums.size();
-        int index = -1;
-
-        // Step 1: Find the first decreasing element from the end
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                index = i;
-                break;
-            }
-        }
-
-        // If no such element is found, reverse the entire array and return
-        if (index == -1) {
-            reverse(nums.begin(), nums.end());
-            return;
-        }
-
-        // Step 2: Find the element just larger than nums[index] to the right of index
-        for (int j = n - 1; j > index; j--) {
-            if (nums[j] > nums[index]) {
-                swap(nums[j], nums[index]);
-                break;
-            }
-        }
-
-        // Step 3: Reverse the elements from index + 1 to the end of the array
-        reverse(nums.begin() + index + 1, nums.end());
-    }
-};
+}; 
