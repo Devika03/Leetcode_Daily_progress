df48a5598d53a9d6a71c0862f151cb5a901dcbdd
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-06-13 00:22:20 +0400

    Time: 41 ms (5.14%), Space: 27.7 MB (6.86%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,23 +1,58 @@
+/*class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        int element=-1;
+        
+        unordered_map<int,int>map;
+        for(int i = 0 ; i<nums.size() ; i++)//0 1 2 3 
+        {
+            map[nums[i]]=0;
+        }
+        for(int i = 0 ; i<nums.size() ; i++)
+        {
+            map[nums[i]]++;
+        }
+        
+        for(int i = 0 ; i<map.size() ; i++)
+        {
+            if(map[i]==0) 
+            {
+                element = i;
+                break;
+            }
+        }
+        return element;
+        
+    }
+};
+*/
 class Solution {
 public:
     int missingNumber(vector<int>& nums) 
     {
         int n = nums.size();
-        int element = -1;
+        int element = 0;
+
+        // Initialize the map to count occurrences
+        unordered_map<int, int> map;
         
-        // Create a boolean array to mark the presence of each number
-        vector<bool> present(n + 1, false);
-        
-        // Mark the presence of each number in the array
-        for(int i = 0; i < n; i++)
+        // Initialize map with all possible numbers from 0 to n with count 0
+        for(int i = 0; i <= n; i++) // Loop goes up to n inclusive
         {
-            present[nums[i]] = true;
+            map[i] = 0;
+        }
+
+        // Count the occurrences of each number in nums
+        for(int i = 0; i < n; i++) // Loop through nums, valid index from 0 to n-1
+        {
+            map[nums[i]]++;
         }
         
-        // Find the first number that is not present
+        // Find the missing number
         for(int i = 0; i <= n; i++)
         {
-            if(!present[i])
+            if(map[i] == 0) 
             {
                 element = i;
                 break;
