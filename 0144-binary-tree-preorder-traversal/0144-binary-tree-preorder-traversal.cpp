190d4f2d3ba0481a212527bbf9d0b456ba5644ba
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-06-20 13:10:14 +0400

    Time: 4 ms (31.34%), Space: 9.9 MB (47.32%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,45 +11,19 @@
  */
 class Solution {
 public:
+    void preorder( TreeNode *root , vector<int> &vec)
+    {
+        if ( root == NULL ) return ;
+        
+        vec.push_back(root->val);
+        preorder( root->left , vec);
+        preorder(root -> right , vec);
+    }
+    
     vector<int> preorderTraversal(TreeNode* root) 
     {
-        //root>>left>>right
-        vector<int>preorder;
-        stack<TreeNode*>st;
-        if(root==NULL)return preorder;
-        st.push(root);
-        while(!st.empty()){ 
-            root=st.top();
-            st.pop();
-            preorder.push_back(root->val);
-
-            if(root->right!=NULL){
-                st.push(root->right);
-            }
-            if(root->left!=NULL){
-                st.push(root->left);
-            }
-        }
-        return preorder;
+        vector<int>result;
+        preorder(root, result);
+        return result;
     }
 };
-/*
-class Solution {
-public:
-    void preorder(TreeNode* root,vector<int>& v) 
-    {
-        if(root==NULL)//If Leaf Node. 
-            return ;
-        
-         v.push_back(root->val);//Root
-         inorder(root->left,v);//L
-         inorder(root->right,v);//R
-    }
-    vector<int> inorderTraversal(TreeNode* root) //To declare v and to return that v. 
-    {
-        vector<int> v;
-        inorder(root,v);
-        return v;
-    }
-};
-*/
