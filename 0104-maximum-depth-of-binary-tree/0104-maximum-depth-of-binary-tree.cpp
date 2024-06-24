c95f5d3e44c18460cf8b57d7e24c4f59f44f2596
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-06-24 18:54:25 +0400

    Time: 8 ms (39.24%), Space: 18.5 MB (11.29%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,12 +11,35 @@
  */
 class Solution {
 public:
+    void helper(TreeNode* node , vector<vector<int>>&ans)
+    {
+        if(node==NULL)return;
+        
+        queue<TreeNode*>q;
+        q.push(node);
+        
+        while(!q.empty())
+        {
+            vector<int>ans_temp;
+            
+            int size = q.size(); //To get the size of the current level. 
+            for(int i = 0 ; i<size ; i++)
+            {
+                TreeNode*temp = q.front();
+                ans_temp.push_back(temp->val);
+                q.pop();
+                
+                if(temp->left!=NULL) q.push(temp->left);
+                if(temp->right !=NULL) q.push(temp->right);
+            }
+            ans.push_back(ans_temp);
+        }
+    }
+    
     int maxDepth(TreeNode* root) 
     {
-        if(root==NULL)return 0;
-        int lh= maxDepth(root->left);
-        int rh=maxDepth(root->right);
-        return 1 + max(lh,rh);
-        
+        vector<vector<int>>result;
+        helper(root,result);
+        return result.size();
     }
 };
