3e3735a2c6c31934de8ae27156bcc168693635e9
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-06-27 13:55:40 +0400

    Time: 3 ms (51.62%), Space: 12.9 MB (88.83%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -11,24 +11,35 @@
  */
 class Solution {
 public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        //similar as level order traversal (we use queue) but here we use flag to change whenever we have to change the direction, and then we vector<vector<int>> to store answer  
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) 
+    {
+       
         vector<vector<int>>ans;
+        
         if(root==NULL) return ans;
+        
+        
         queue<TreeNode*>q;
         q.push(root);
-        int flag=1;
-        while(!q.empty()){
+        
+        int flag=1;//Current will be L to R ,next change to 0 to indicate R to L .
+        
+        while(!q.empty())
+        {
 
             int size=q.size();//stores the size of queue
             vector<int>temp(size);//temp vector stores the all nodes value of particular level 
-            for(int i=0;i<size;i++){//i=0 to size
+            
+            for(int i=0;i<size;i++)
+            {
+                //i=0 to size
                 //storing the front most element of queue in node
                 TreeNode*node=q.front();
                 //popping the front elemenet in queue
                 q.pop();
 
-                if(flag==1){
+                if(flag==1)
+                {
                     //then store left to right
                    temp[i]=node->val;
                 }else{
@@ -39,9 +50,9 @@ public:
                 if(node->left!=NULL) q.push(node->left);
                 if(node->right!=NULL) q.push(node->right);
             }
-            //if flag==1 then left to right, now we change to 0 to indicate its time for right to left, similarly vice versa
+            
             flag=!flag;
-            //pusshing the temp vector
+            
             ans.push_back(temp);
         }
         return ans;
