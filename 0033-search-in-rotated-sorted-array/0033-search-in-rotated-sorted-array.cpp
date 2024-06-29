d19d503fffd0377763793fb31973a84447be2a7e
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-06-29 14:53:32 +0400

    Time: 0 ms (100.00%), Space: 13.7 MB (6.23%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,13 +1,52 @@
-class Solution {
+class Solution 
+{
 public:
-    int search(vector<int>& nums, int target) {
-        int v=-1;
-        for(int i=0;i<nums.size();i++){
-            if(nums.at(i)==target){
+    int search(vector<int>& nums, int target) 
+    {
+        int low = 0 ; 
+        int high = nums.size()-1;
+        
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            
+            if(nums[mid]==target) return mid; 
+            
+            //So we Check both halves of nums . 
+            
+            if(nums[low]<=nums[mid])
+            {
+                if(target>= nums[low] && target< nums[mid])
+                {
+                    high = mid-1;
+                }
+                else {low=mid+1;}
+            }
+            else 
+            {
+                if(target> nums[mid] && target<=nums[high])
+                {
+                    low = mid+1;
+                }
+                else { high = mid-1; }
+            }
+            
+        }
+        return -1;    
+    }
+};
+
+
+/*
+Naive Approach : 
+int v=-1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums.at(i)==target)
+            {
                 v=i;
                 break;
             }
         }
-        return v;
-    }
-};
+        return v ;
+*/
