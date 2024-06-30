8a72bbf89b0a9ddb4ad8c3a064f8044317b20f1a
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-06-30 12:37:27 +0400

    Time: 18 ms (87.01%), Space: 95.3 MB (33.41%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,24 +1,93 @@
-class Solution
-{
-    public:
-double findMedianSortedArrays(vector<int>& l, vector<int>& r) {
-    int li = 0, ri = 0;
-    auto extractNext = [&]() -> int {
-        if (li == l.size()) return r[ri++];
-        if (ri == r.size()) return l[li++];
-        return l[li] < r[ri] ? l[li++]: r[ri++];
-    };
-            
-    int total = l.size() + r.size(), targetIndex = (total - 1) / 2, currentItem = 0;
-    bool isOdd = (total & 1);
-    
-    for (int i = 0; i < total; i++) {
-        currentItem = extractNext();
-        if ( i < targetIndex ) continue;
+// class Solution 
+// {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+//     {
+//         double median = 0;
+//         vector<int>result;
+//         for(int i = 0 ; i<nums1.size() ; i++)
+//         {
+//             result.push_back(nums1[i]);
+//         }
+//         for(int i = 0 ; i<nums2.size() ; i++)
+//         {
+//             result.push_back(nums2[i]);
+//         }
+//         sort(result.begin(),result.end());
         
-        if (isOdd) return currentItem;
-        else return (currentItem + extractNext()) / 2.0;
+//         if(result.size()%2==0)
+//         {
+//             int left = 0 ; 
+//             int right = result.size()-1;
+//             int index_1 = ( left + right ) / 2;
+//             int index_2 = index_1 +1 ; 
+//             median = (result[index_1] + result[index_2])/2;
+//         }
+//         else{
+//             int left = 0 ; 
+//             int right = result.size()-1;
+//             int index_1 = ( left + right ) / 2;
+//             median = result[index_1];
+//         }
+        
+//         return median;
+        
+//     }
+// };
+
+
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution 
+{
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+    {
+        vector<int> merged;
+        int i = 0, j = 0;
+        
+        // Merge the two sorted arrays into the merged vector
+        while (i < nums1.size() && j < nums2.size()) 
+        {
+            if (nums1[i] < nums2[j]) 
+            {
+                merged.push_back(nums1[i]);
+                i++;
+            } 
+            else 
+            {
+                merged.push_back(nums2[j]);
+                j++;
+            }
+        }
+        
+        // Append any remaining elements from nums1
+        while (i < nums1.size()) 
+        {
+            merged.push_back(nums1[i]);
+            i++;
+        }
+        
+        // Append any remaining elements from nums2
+        while (j < nums2.size()) 
+        {
+            merged.push_back(nums2[j]);
+            j++;
+        }
+        
+        int n = merged.size();
+        
+        // Calculate the median
+        if (n % 2 == 0) 
+        {
+            return (merged[n/2 - 1] + merged[n/2]) / 2.0;
+        } 
+        else 
+        {
+            return merged[n/2];
+        }
     }
-    return 0;
-}
 };
