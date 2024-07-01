39a0a3ec7c148fa92b4620a352d7aefc605ee6e8
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-07-01 12:44:43 +0400

    Time: 16 ms (91.38%), Space: 94.5 MB (57.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,58 +1,125 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size();
-        int n2=nums2.size();
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+    {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
         
-        int count_ptr=0;
+        int n = n1+n2;
+        int index1 = n/2;
+        int index2 = index1-1;
+        int count = 0 ; 
         
-        int i=0,j=0;
+        int val1 = -1; 
+        int val2 = -1;
         
-        int n=n1+n2;
-        
-        int mid_right=n/2;
-        int mid_left=mid_right-1;
-        
-        int val1_at_mid_right=-1,val2_at_mid_left=-1;
-        
+        int i = 0 ; int j = 0 ; 
         while(i<n1 && j<n2)
         {
             if(nums1[i]<nums2[j])
             {
-                if(count_ptr==mid_left)val1_at_mid_right=nums1[i];
-                if(count_ptr==mid_right)val2_at_mid_left=nums1[i];
-                count_ptr++;
+                if(count==index1) val1 = nums1[i];
+                if(count==index2) val2 = nums1[i];
+                
+                count++;
                 i++;
+                
             }
-            else{
-                if(count_ptr==mid_left)val1_at_mid_right=nums2[j];
-                if(count_ptr==mid_right)val2_at_mid_left=nums2[j];
-                count_ptr++;
+            else
+            {
+                if(count==index1) val1 = nums2[j];
+                if(count==index2) val2 = nums2[j];
+                
+                count++;
                 j++;
             }
-            
         }
-        while (i < n1) {
-        if (count_ptr == mid_left)val1_at_mid_right = nums1[i];
-        if (count_ptr == mid_right)val2_at_mid_left = nums1[i];
-        count_ptr++;
-        i++;
-    }
-    while (j < n2) {
-        if (count_ptr == mid_left)val1_at_mid_right = nums2[j];
-        if (count_ptr == mid_right)val2_at_mid_left = nums2[j];
-        count_ptr++;
-        j++;
-    }
-    if(n%2==0){
-        return (double)(val1_at_mid_right+val2_at_mid_left)/2;
-    }else{
-        return val2_at_mid_left;
-    }
-
-        return -1;
+        //Left-Over elements of either of the 2 arrays : 
+        while (i < n1) 
+        {
+            if (count == index1)val1 = nums1[i];
+            if (count == index2)val2 = nums1[i];
+            count++;
+            i++;
+        }
+        while (j < n2) 
+        {
+            if (count == index1)val1 = nums2[j];
+            if (count == index2)val2 = nums2[j];
+            count++;
+            j++;
+        }
+        if(n%2 != 0) return val1;
+        else return (double)(val1+val2)/2;
+        
+    
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int n1=nums1.size();
+//         int n2=nums2.size();
+        
+//         int count_ptr=0;
+        
+//         int i=0,j=0;
+        
+//         int n=n1+n2;
+        
+//         int mid_right=n/2;
+//         int mid_left=mid_right-1;
+        
+//         int val1_at_mid_right=-1,val2_at_mid_left=-1;
+        
+//         while(i<n1 && j<n2)
+//         {
+//             if(nums1[i]<nums2[j])
+//             {
+//                 if(count_ptr==mid_left)val1_at_mid_right=nums1[i];
+//                 if(count_ptr==mid_right)val2_at_mid_left=nums1[i];
+//                 count_ptr++;
+//                 i++;
+//             }
+//             else{
+//                 if(count_ptr==mid_left)val1_at_mid_right=nums2[j];
+//                 if(count_ptr==mid_right)val2_at_mid_left=nums2[j];
+//                 count_ptr++;
+//                 j++;
+//             }
+            
+//         }
+//         while (i < n1) 
+//         {
+//         if (count_ptr == mid_left)val1_at_mid_right = nums1[i];
+//         if (count_ptr == mid_right)val2_at_mid_left = nums1[i];
+//         count_ptr++;
+//         i++;
+//         }
+//     while (j < n2) {
+//         if (count_ptr == mid_left)val1_at_mid_right = nums2[j];
+//         if (count_ptr == mid_right)val2_at_mid_left = nums2[j];
+//         count_ptr++;
+//         j++;
+//     }
+//     if(n%2==0){
+//         return (double)(val1_at_mid_right+val2_at_mid_left)/2;
+//     }else{
+//         return val2_at_mid_left;
+//     }
+
+//         return -1;
+    
 
 /*
 Naive Approach : 
