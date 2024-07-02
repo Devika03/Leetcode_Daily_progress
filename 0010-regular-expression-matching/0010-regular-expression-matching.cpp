4ce42c3520fb7944a59ec85d6e772a10d99d15cf
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-07-02 15:58:29 +0400

    Time: 11 ms (21.12%), Space: 20 MB (7.90%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+
+    bool solve(int index1,int index2,string s,string p,vector<vector<int>>&dp)
+    {
+        if(index1<0 && index2<0)
+        {
+            return true;
+        }
+
+        if(index1<0)
+        {
+            while(index2>=0)
+            {
+                if(p[index2]!='*')
+                {
+                    return false;
+                }
+                index2-=2;
+            }
+            return true;
+        }
+
+        if(index1>=0 && index2<0)
+        {
+            return false;
+        }
+
+        if(dp[index1][index2]!=-1)
+        {
+            return dp[index1][index2];
+        }
+
+        if(s[index1]==p[index2] || p[index2]=='.')
+        {
+            dp[index1][index2]=solve(index1-1,index2-1,s,p,dp);
+        }
+        else if(p[index2]=='*')
+        {
+            bool zero=solve(index1,index2-2,s,p,dp);
+            bool one=(index1>=0 && (s[index1]==p[index2-1] || p[index2-1]=='.')) &&
+                        solve(index1-1,index2,s,p,dp);
+            
+            dp[index1][index2]=zero||one;
+        }
+        else
+        {
+            dp[index1][index2]=false;
+        }
+        return dp[index1][index2];
+    }
+
+    bool isMatch(string s, string p) {
+        int n=s.size();
+        int m=p.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+
+        return solve(n-1,m-1,s,p,dp);
+    }
+};
