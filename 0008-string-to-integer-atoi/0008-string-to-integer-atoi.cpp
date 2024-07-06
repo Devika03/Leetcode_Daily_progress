8f5bd4a2da19cdeeb89e1d10565cf42a6942820c
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-07-06 11:50:28 +0400

    Time: 3 ms (58.05%), Space: 8.4 MB (16.45%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,83 +1,45 @@
-/*class Solution {
+class Solution {
 public:
     int myAtoi(string s) 
     {
-        string result = "";
-        int result_int = 0;
-        int flag = 0;//For no sign and "+"
-        for(int i = 0 ; i<s.size() ; i++)
+        int i = 0 ;
+        
+        while(i<s.size() && s[i]==' ')
         {
-            while(s[i] == ' ')
-            {
-                i++;
-            }
-            
-            if(s[i]=='-') 
-            {
-                flag=1;
-                i++;
-            }
-            while(s[i]=='0')
-            {
-                i++;
-            }
-            if(i!=s.size())
-            {
-                result =result+s[i];
-            }
-            
-            result_int = stoi(result);
-                        
+            i++;
         }
-        if(flag==1)
+        s = s.substr(i);
+        
+        int flag = 1;
+        int skip_positivesign = 0;
+        
+        long ans = 0 ; 
+        
+        if(s[0]=='-') flag = -1;
+        if(s[0]=='+') skip_positivesign=1;
+        
+        if(flag==1 && skip_positivesign==0 )
         {
-            result_int = -result_int;
+            i=0;
         }
-         return result_int; 
-    }
-};*/
-
-#include <string>
-
-using namespace std;
-
-class Solution {
-public:
-    int myAtoi(string s) {
-        long long result_int = 0; // Change int to long long
-        int flag = 1; // Initialize flag to 1 for default positive sign
-        int i = 0;
-
-        // Skip leading spaces
-        while (s[i] == ' ') {
-            i++;
+        else 
+        {
+            i=1;
         }
-
-        // Check for optional sign
-        if (s[i] == '-') {
-            flag = -1;
-            i++;
-        } else if (s[i] == '+') {
-            i++;
-        }
-
-        // Accumulate digits
-        while (isdigit(s[i])) {
-            result_int = result_int * 10 + (s[i] - '0');
-            // Check for out-of-range condition
-            if (flag == 1 && result_int > INT_MAX) {
-                return INT_MAX;
-            } else if (flag == -1 && -result_int < INT_MIN) {
-                return INT_MIN;
+        
+        while(i<s.size())
+        {
+            if(s[i]==' ' || !isdigit(s[i])) break;
+            
+            else
+            {
+                ans = ans*10 + s[i]-'0';
+                
+                if(flag==-1 && flag*ans<INT_MIN) return INT_MIN;
+                if(flag==1 && flag*ans>INT_MAX) return INT_MAX;
+                i++;
             }
-            i++;
         }
-
-        // Apply sign
-        result_int *= flag;
-
-        return static_cast<int>(result_int);
+        return (int)(flag*ans);
     }
 };
-
-
