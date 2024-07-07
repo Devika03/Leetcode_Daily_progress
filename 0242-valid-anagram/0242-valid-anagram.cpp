ea989cd284240bd3ac3b2fb7f88997aa86322593
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-07-07 15:06:26 +0400

    Time: 6 ms (79.78%), Space: 9.8 MB (19.40%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,33 +1,71 @@
-#include<string.h>
-
 class Solution 
 {
 public:
-    bool isAnagram(string s, string t) 
+    bool isAnagram(string s, string t) //t , s .
     {
-        int s_len = s.size();
-        int t_len = t.size();
-        bool flag = true;
-        if(s_len!=t_len)
+        if(s.size()<t.size()) return false;
+        unordered_map<char,int>char_count_s;
+        unordered_map<char,int>t_char_count;
+        for(int i = 0 ; i<s.size() ; i++)
         {
-            return false;
-        }
-        else
-        {
-            for( int i = 0 ; i < t_len ; i++)
+            if(char_count_s.find(s[i])==char_count_s.end())
             {
-                if(s.find(t[i])!=string::npos)
-                {
-                    s.erase(s.find(t[i]),1);
-                    continue;
-                }
-                else
-                {
-                    flag = false;
-                    break;
-                }
+                char_count_s[s[i]]=1;
+            }
+            char_count_s[s[i]]++;            
+        }
+        
+        for(int i = 0 ; i<t.size() ; i++)
+        {
+            if(t_char_count.find(t[i])==t_char_count.end())
+            {
+                t_char_count[t[i]]=1;
+            }
+            t_char_count[t[i]]++;            
+        }
+        bool t_is_anagram = true;
+        for(auto it:char_count_s)
+        {
+            if(t_char_count.find(it.first)!=t_char_count.end() && char_count_s[it.first]==t_char_count[it.first])
+            {
+                continue;
+            }
+            else
+            {
+                t_is_anagram=false;
+                break;
             }
         }
-        return flag;
+        return t_is_anagram; 
+        
+        
     }
 };
+
+
+
+
+// int s_len = s.size();
+//         int t_len = t.size();
+//         bool flag = true;
+//         if(s_len!=t_len)
+//         {
+//             return false;
+//         }
+//         else
+//         {
+//             for( int i = 0 ; i < t_len ; i++)
+//             {
+//                 if(s.find(t[i])!=string::npos)
+//                 {
+//                     s.erase(s.find(t[i]),1);
+//                     continue;
+//                 }
+//                 else
+//                 {
+//                     flag = false;
+//                     break;
+//                 }
+//             }
+//         }
+//         return flag;
