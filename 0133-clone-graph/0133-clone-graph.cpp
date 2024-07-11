a0c4777d46083f917f38aa737cb7462341b81f23
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-07-11 18:33:20 +0400

    Time: 4 ms (38.90%), Space: 11.7 MB (19.07%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -22,41 +22,79 @@ public:
 
 
 
-class Solution {
+class Solution 
+{
 public:
     Node* cloneGraph(Node* node) 
     {
-        if (!node) return nullptr;
-
-        unordered_map<Node*, Node*> visited;
-        queue<Node*> q;
+        if (node==NULL) return NULL;
+        
+        unordered_map<Node*,Node*>Original_Clone;
+        queue<Node*>q;
+        
         q.push(node);
-
-        // Clone the root node and store it in visited map
+        
         Node* temp = new Node(node->val);
-        visited[node] = temp;
-
-        while (!q.empty()) {
-            Node* currNode = q.front();
+        Original_Clone[node]=temp;
+        
+        while(!q.empty())
+        {
+            Node* current_o_node = q.front();
             q.pop();
-
-            for (Node* neighbor : currNode->neighbors) 
+            
+            for(auto it :current_o_node->neighbors)
             {
-                if (visited.find(neighbor) == visited.end()) {
-                    // Clone the neighbor node and store it in visited map
-                    temp = new Node(neighbor->val);
-                    visited[neighbor] = temp;
-                    q.push(neighbor);
+                if(Original_Clone.find(it)==Original_Clone.end())
+                {
+                    Node*temp = new Node(it->val);
+                    Original_Clone[it]=temp;
+                    
+                    q.push(it);
                 }
-                // Connect the clone of the current node to the clone of the neighbor
-                visited[currNode]->neighbors.push_back(visited[neighbor]);
+                //At the same time , We need to also update neighbors array for the clone graph :
+                Original_Clone[current_o_node]->neighbors.push_back(Original_Clone[it]);
+                
             }
         }
+        return Original_Clone[node];
+        
+        
 
-        return visited[node];
+        
 
     }
 };
+
+
+
+
+// unordered_map<Node*, Node*> visited;
+//         queue<Node*> q;
+        
+//         q.push(node);
+
+//         // Clone the root node and store it in visited map
+//         Node* temp = new Node(node->val);
+//         visited[node] = temp;
+
+//         while (!q.empty()) {
+//             Node* currNode = q.front();
+//             q.pop();
+
+//             for (Node* neighbor : currNode->neighbors) 
+//             {
+//                 if (visited.find(neighbor) == visited.end()) {
+//                     // Clone the neighbor node and store it in visited map
+//                     temp = new Node(neighbor->val);
+//                     visited[neighbor] = temp;
+//                     q.push(neighbor);
+//                 }
+//                 // Connect the clone of the current node to the clone of the neighbor
+//                 visited[currNode]->neighbors.push_back(visited[neighbor]);
+//             }
+//         }
+
+//         return visited[node];
 
 
 
