f5027e46096965883300fed61a0d158ab5fe2fce
Devika Simlai <91256929+Devika03@users.noreply.github.com>
2024-08-13 21:51:55 +0530

    Time: 23 ms (18.49%), Space: 16.7 MB (29.68%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,48 +1,99 @@
-class Solution 
-{
-    private:
-    bool bfs(int value , vector<vector<int>> &grid , vector<int> &visited)
-{
-    queue<int> q;
-    q.push(value);//Node 
-    visited[value] = 0;
-    
-    while(!q.empty())
-    {
-        vector<int> temp = grid[q.front()];//?
-        int val = q.front();
-        q.pop();
+// class Solution {
+// public:
+//     void BFS(vector<vector<int>>& graph , int source , vector<int>&color , vector<int>&visited , bool &is_Bip )
+//     {
+//         int col_1 = 0 ; 
+//         int col_2 = 1 ; 
         
-        for(int i=0 ;i<temp.size() ;i++)
-        {
-                if(visited[temp[i]] == -1)
-                {
-                    visited[temp[i]] = !visited[val];
-                    q.push(temp[i]);
-                }
-            else if(visited[temp[i]]  == visited[val])
-                return false;
-        }
-    }
-    return true;//if q==Empty
-}
+//         queue<int>q;
+        
+//         visited[source]= 1;
+//         color[source]= col_1; int now_col =col_2;
+//         q.push(source);
+        
+//         while(!q.empty())
+//         {
+//             int element = q.front();
+//             q.pop();
+//             for(auto it : graph[element])
+//             {
+//                 if(visited[it]==0)
+//                 {
+//                     visited[it]=1;
+//                     color[it]=now_col;
+//                     now_col= col_1;
+//                 }
+//                 if(visited[it]==1 && color[it]==now_col)
+//                 {
+//                     is_Bip = false;
+//                     break;
+//                 }
+//             }
+//         }
+        
+//     }
+    
+    
+//     bool isBipartite(vector<vector<int>>& graph) 
+//     {
+//         int n = graph.size();
+//         vector<int>color(n,-1);
+//         vector<int>visited(n,0);
+        
+//         int source = 0 ;
+//         bool is_Bip = true;
+//         BFS(graph, source , color , visited , is_Bip);
+//         return is_Bip;
+        
+        
+        
+//     }
+// };
 
 
 
+class Solution {
 public:
-bool isBipartite(vector<vector<int>>& graph) {
-    int n = graph.size();
-    vector<int> visited(n ,-1);//Vis. Array.
-    for(int i= 0; i<n ;i++)
-    {
-        if(visited[i] == -1)//If not vis. i.e -1.
-        {
-            if(bfs(i , graph , visited) == false)
-                return false;
+    bool BFS(vector<vector<int>>& graph, int source, vector<int>& color) {
+        int col_1 = 0; 
+        int col_2 = 1;
+        
+        queue<int> q;
+        q.push(source);
+        color[source] = col_1;
+        
+        while (!q.empty()) {
+            int element = q.front();
+            q.pop();
             
+            for (auto it : graph[element]) {
+                if (color[it] == -1) {
+                    // If the adjacent node is not colored, assign it the opposite color
+                    color[it] = 1 - color[element];
+                    q.push(it);
+                } else if (color[it] == color[element]) {
+                    // If the adjacent node has the same color, the graph is not bipartite
+                    return false;
+                }
+            }
         }
+        
+        return true;
     }
-    return true;
-
-}
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n, -1);  // Initialize all nodes as uncolored
+        
+        // Perform BFS for every component of the graph
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {  // If the node is uncolored, start BFS from there
+                if (!BFS(graph, i, color)) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;  // If no conflicts found, graph is bipartite
+    }
 };
